090daa0ba987ccb40e09b06334644532a52f3349
343dev <343dev@users.noreply.github.com>
2025-04-10 12:18:33 +0700

    Update guetzli binaries

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import path from 'node:path';
+import { arch, platform } from 'node:process';
 import { fileURLToPath } from 'node:url';
-
-const BINARY_NAME = {
-	darwin: 'guetzli_darwin_x86-64',
-	linux: 'guetzli_linux_x86-64',
-	win32: 'guetzli_windows_x86-64.exe',
-};
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-const binaryName = BINARY_NAME[process.platform];
-const binaryPath = path.join(dirname, 'vendor', binaryName);
+const binaryName = `guetzli_${arch}${platform === 'win32' ? '.exe' : ''}`;
+const binaryPath = path.join(dirname, 'vendor', platform, binaryName);
 
 export default binaryPath;
