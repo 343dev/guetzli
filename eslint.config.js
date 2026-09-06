4d7fe1f43ba1e491e5a98f6b0a90302a7fb36647
343dev <343dev@users.noreply.github.com>
2026-09-06 09:58:25 +0700

    build(deps): update and pin development dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -9,4 +9,21 @@ export default [
 		],
 	},
 	...config,
+	{
+		rules: {
+			'unicorn/consistent-boolean-name': 'off',
+			'unicorn/no-computed-property-existence-check': 'off',
+			'unicorn/no-negated-array-predicate': 'off',
+			'unicorn/no-return-array-push': 'off',
+			'unicorn/no-top-level-assignment-in-function': 'off',
+			'unicorn/prefer-await': 'off',
+			'unicorn/prefer-continue': 'off',
+			'unicorn/prefer-early-return': 'off',
+			'unicorn/prefer-iterator-to-array': 'off',
+			'unicorn/prefer-minimal-ternary': 'off',
+			'unicorn/prefer-number-is-safe-integer': 'off',
+			'unicorn/prefer-promise-with-resolvers': 'off',
+			'unicorn/require-array-sort-compare': 'off',
+		},
+	},
 ];
